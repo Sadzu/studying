6daa728b0cd2df4a1d5be80058d1977159e9291c
sadzu <miraiku@vk.com>
2023-11-18 13:58:29 +0700

    lab_6

---
+++ lab_6/Header_files/headers.h
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <cstring>
+#include <fstream>
+
+#pragma once
+
+using namespace std;
+
+class Matrix {
+ public:
+    Matrix();
+    Matrix(int a, int **matrix);
+    Matrix(const Matrix &);
+
+    size_t getMatrixOrder();
+    int **getlines();
+
+    void EnterMatrix(int a);
+    virtual void print();
+    void transportation();
+    Matrix operator +(Matrix matrix);
+    friend Matrix operator -(Matrix matrix_1, Matrix matrix_2);
+    void operator =(Matrix matrix);
+    Matrix operator ++();
+    Matrix operator ++(int);
+    operator int() const;
+    friend ifstream& operator >>(ifstream &is, Matrix &matrix);
+    friend ofstream& operator <<(ofstream &os, Matrix &matrix);
+    friend fstream& operator << (fstream &in, Matrix &matrix);
+    void change_value(size_t x, size_t y, int value);
+
+    virtual ~Matrix();
+
+ private:
+    size_t MatrixOrder;
+    int **lines;
+
+};
+
+class Matrix_with_name: public Matrix {
+ private:
+   char *Name;
+ public:
+   Matrix_with_name(int order, int **matrix, char *name);
+   Matrix_with_name(const Matrix_with_name &);
+   Matrix_with_name();
+
+   void print();
+   Matrix_with_name operator +(Matrix_with_name matrix);
+   void ChangeName(char *new_name);
+   //void operator =(Matrix_with_name &matrix);
+
+   ~Matrix_with_name();
+};
+
+class Matrix_with_int : public Matrix {
+ private:
+   int MatrixNumber;
+ public:
+   Matrix_with_int(int order, int **matrix, int number);
+   Matrix_with_int(const Matrix_with_int &);
+   Matrix_with_int();
+
+   void print();
+
+   ~Matrix_with_int();
+};
+
+Matrix operator -(Matrix matrix_1, Matrix matrix_2);
+ofstream& operator <<(ofstream &os, Matrix &matrix);
+ifstream& operator >>(ifstream &is, Matrix &matrix);
+fstream& operator << (fstream &in, Matrix &matrix);
---
+++ lab_6/Header_files/queue_list.h
@@ -0,0 +1,29 @@
+#include "headers.h"
+
+#pragma once
+
+struct Node {
+    Node *next;
+    Node *prev;
+    Matrix *data;
+    unsigned number;
+};
+
+class queue_list {
+ private:
+    Node *head;
+    Node *last;
+ public:
+    queue_list();
+
+    void Delete();
+    void Add(Matrix &matrix);
+    //void Delete_by_number(int number);
+    //void Add_by_number(int number);
+    void print();
+    unsigned search(Matrix &matrix);
+    Matrix* get_by_number(unsigned num);
+    unsigned GetSize();
+
+    ~queue_list();
+};
