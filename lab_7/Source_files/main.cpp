0e51e07237d1f45adcef92bd7d28c035dd1d57e4
sadzu <miraiku@vk.com>
2023-12-05 16:19:00 +0700

    lab_7 fixed

--- lab_7/Source_files/main.cpp
+++ lab_7/Source_files/main.cpp
@@ -1,4 +1,3 @@
-#include "../Header_files/cyclic_queue.h"
 #include "../Header_files/cyclic_queue_funcs.h"
 
 using namespace std;
@@ -16,8 +15,7 @@ int main() {
             break;
         }
 
-        switch (flag)
-        {
+        switch (flag) {
         case -1:
             break;
         
@@ -38,21 +36,14 @@ int main() {
 }
 
 void char_test() {
-    cout << "How long queue for strings?: ";
-    size_t string_queue_size;
-    cin >> string_queue_size;
-    
-    cyclic_queue<char> string_queue = cyclic_queue<char>(string_queue_size);
-
+    cout << "Enter strings: \n";
     char *tmp = new char[50];
-    cout << "Enter string: ";
+    char *tmp_2 = new char[50];
     cin >> tmp;
+    cin >> tmp_2;
+    cyclic_queue<char> string_queue = cyclic_queue<char>(2);
     string_queue.enQueue(tmp);
-    string_queue.print();
-    cout << "One more: ";
-    char *tmp_1 = new char[100];
-    cin >> tmp_1;
-    string_queue.enQueue(tmp_1);
+    string_queue.enQueue(tmp_2);
     string_queue.print();
     string_queue.deQueue();
     string_queue.deQueue();
@@ -64,19 +55,15 @@ void int_test() {
     size_t int_queue_size;
     cin >> int_queue_size;
     
-    cyclic_queue<int> string_queue = cyclic_queue<int>(int_queue_size);
+    cyclic_queue<int> int_queue = cyclic_queue<int>(int_queue_size);
 
-    int tmp;
-    cout << "Enter number: ";
-    cin >> tmp;
-    string_queue.enQueue(&tmp);
-    string_queue.print();
-    cout << "One more: ";
-    int tmp_1;
-    cin >> tmp_1;
-    string_queue.enQueue(&tmp_1);
-    string_queue.print();
-    string_queue.deQueue();
-    string_queue.deQueue();
-    string_queue.print();
+    int *tmp = new int[int_queue_size];
+    for (size_t i = 0; i < int_queue_size; i++) {
+        cin >> tmp[i];
+        int_queue.enQueue(&tmp[i]);
+    }
+    int_queue.print();
+    for (size_t i = 0; i < int_queue_size; i++) {
+        int_queue.deQueue();
+    }
 }
